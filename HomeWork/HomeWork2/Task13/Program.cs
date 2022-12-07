9686612ec3ac4d76129f5ed7d7856149282e826d
Maria <ru4kina@yandex.ru>
2022-12-07 21:00:57 +0300

    Task13 added

--- HomeWork/HomeWork2/Task13/Program.cs
+++ HomeWork/HomeWork2/Task13/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+Console.WriteLine("Введите число: ");
+long n = Convert.ToInt64(Console.ReadLine());
+
+if (n<100) Console.WriteLine("Третьей цифры нет");
+else 
+{
+    while (n>1000) n=n/10;
+    n=n%10;
+    Console.WriteLine($"Третья цифра: {n}");
+}
+
