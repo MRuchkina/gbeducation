18f268f00290dd2d035cbf9ef5345d96ea958f38
Maria <ru4kina@yandex.ru>
2022-12-07 20:48:51 +0300

    Task10  made

--- HomeWork/HomeWork1/Task10/Program.cs
+++ HomeWork/HomeWork1/Task10/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+
+int num = new Random().Next(100,1000);
+Console.WriteLine($"Сгенерировано число {num}");
+
+num = num/10;
+int res = num%10;
+
+Console.WriteLine($"Вторая цифра числа: {res}");
