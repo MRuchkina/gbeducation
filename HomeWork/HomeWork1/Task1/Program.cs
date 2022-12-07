4b06445146221ddca73cb5baa2252b99613b1cb4
Maria <ru4kina@yandex.ru>
2022-12-07 18:54:02 +0300

    added Task2

--- HomeWork/HomeWork1/Task1/Program.cs
+++ HomeWork/HomeWork1/Task1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int min;
+int max;
+
+if (a>b)
+{
+  min = b;
+  max = a;
+}
+else
+{
+  min = a;
+  max = b;
+}
+
+Console.Write("min: ");
+Console.WriteLine(min);
+
+
+Console.Write("max: ");
+Console.WriteLine(max);
