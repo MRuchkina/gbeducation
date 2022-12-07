c8033205db1fd6ef3836a8e87e4f6770d1e76e80
Maria <ru4kina@yandex.ru>
2022-12-07 19:14:48 +0300

    added Task4

--- HomeWork/HomeWork1/Task3/Program.cs
+++ HomeWork/HomeWork1/Task3/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int b = a/2;
+int c = b*2;
+
+Console.Write("Четное: ");
+
+if(c==a)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
