45d0598ba6e7f6e9bbf052419343b97ece249ff6
Maria <ru4kina@yandex.ru>
2022-12-07 20:24:50 +0300

    Practice2 added

---
+++ PracticeDel/Lesson2/Deleted/Program.cs
@@ -0,0 +1,7 @@
+﻿int num = new Random().Next(10,100);
+Console.WriteLine($"Сгенерировано число {num}");
+int max = num/10;
+if (num%10>max) max = num%10;
+Console.WriteLine($"Наибольная цифра в этом числе это {max}");
+
+for (int i=0; i<=num; i++) Console.Write($"{i} ");
