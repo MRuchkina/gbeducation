a4668ff56191f4bb555763dad44a0c5c0f51fa09
Maria <ru4kina@yandex.ru>
2022-12-07 18:43:07 +0300

    HoneWork added

--- Lecture1/Exercise5/Program.cs
+++ Lecture1/Exercise5/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+int xa = 1, ya = 1;
+int xb = 1, yb = 15;
+int xc = 80, yc = 15;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+
