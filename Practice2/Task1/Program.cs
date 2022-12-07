a545ff28b643d143e5b96247a527005f7ce612da
Maria <ru4kina@yandex.ru>
2022-12-07 20:44:47 +0300

    Task10  added

--- Practice2/Task1/Program.cs
+++ Practice2/Task1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num = new Random().Next(10,100);
+Console.WriteLine($"Сгенерировано число {num}");
+int max = num/10;
+if (num%10>max) max = num%10;
+Console.WriteLine($"Наибольная цифра в этом числе это {max}");
+
+for (int i=0; i<=num; i++) Console.Write($"{i} ");
